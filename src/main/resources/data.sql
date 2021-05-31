0f3bd9da92768dc66bf804d90daf5239d6efd641
argmishra <argmishra.ece@gmail.com>
2021-05-31 01:27:49 +0100

    Database script automation

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT into user (first_name, last_name, start_date, age, active) values
+('Anurag', 'Mishra', now(), 29, true),
+('Himani', 'Sharma', '2020-05-30', 27, false),
+('Abhinav', 'Sharma', '2020-08-25', 33, true),
+('Abhitab', 'Dadhich', '2021-03-05', 32, false),
+('Arvind', 'Singh', '2021-04-25', 16, true),
+('Abhishek', 'Barret', '2021-05-24', 23, false);
